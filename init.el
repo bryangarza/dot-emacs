fcfc607e5df93d9762420b5063846483a0121969
Bryan Garza <brygarza@gmail.com>
2014-11-07 20:35:11 -0800

    initial commit

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'cl)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(menu-bar-mode -1)
+(global-linum-mode 1) ; display line numbers
+(column-number-mode 1) ; display column and row of cursor in mode-line
+
+(add-hook 'text-mode-hook
+	  (lambda ()
+            ;; ask to turn on hard line wrapping
+            (when (y-or-n-p "Hard wrap text?")
+              (turn-on-auto-fill))))
+
+(require 'evil)
+(evil-mode 1)
+
+;; change mode-line color by evil state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+				   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+	    (lambda ()
+	      (let ((color (cond ((minibufferp) default-color)
+				 ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+				 ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+				 ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+				 (t default-color))))
+		(set-face-background 'mode-line (car color))
+		(set-face-foreground 'mode-line (cdr color))))))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'noctilux t)
