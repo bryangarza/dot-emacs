aa76da09346ede46dab92241db94f28dd0b6f70e
Bryan Garza <brygarza@gmail.com>
2014-11-22 14:07:55 -0800

    customizing for evil and clojure

--- init.el
+++ init.el
@@ -1,8 +1,26 @@
 (require 'cl)
 (require 'package)
+
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(evil paredit
+		      evil-paredit evil-surround
+		      rainbow-delimiters
+		      ;; http://www.emacswiki.org/emacs/PareditCheatsheet
+		      ;; http://mumble.net/~campbell/emacs/paredit.html
+		      smartparens
+		      clojure-mode clojure-mode-extra-font-locking
+		      cider
+		      magit))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 (menu-bar-mode -1)
 (global-linum-mode 1) ; display line numbers
@@ -16,6 +34,8 @@
 
 (require 'evil)
 (evil-mode 1)
+(global-rainbow-delimiters-mode t)
+(setq electric-indent-mode nil)
 
 ;; change mode-line color by evil state
 (lexical-let ((default-color (cons (face-background 'mode-line)
@@ -31,9 +51,17 @@
 		(set-face-foreground 'mode-line (cdr color))))))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/customizations")
+
 (load-theme 'noctilux t)
 
 (let ((default-directory "~/.emacs.d/lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 
 (require 'navigate)
+
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(load "elisp-editing.el")
+(load "setup-clojure.el")
