772ca89bee6a46e0cdeb5b867fd877bc8b2c68e2
Bryan Garza <brygarza@gmail.com>
2015-02-24 09:01:42 -0800

    Split below and split right.
    This switches focus to the newly created split when creating one.

--- init.el
+++ init.el
@@ -210,6 +210,9 @@
 (global-set-key [(super d)] 'next-history-element)
 (global-set-key [(super shift k)] 'kill-this-buffer)
 
+(global-set-key "\C-x2" (lambda () (interactive) (split-window-vertically) (other-window 1)))
+(global-set-key "\C-x3" (lambda () (interactive) (split-window-horizontally) (other-window 1)))
+
 (setq ring-bell-function 'ignore)
 (setq-default show-trailing-whitespace t)
 
