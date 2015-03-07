ae65a54b784b38785f56b3216a05e522b0b49e9a
Bryan Garza <brygarza@gmail.com>
2015-03-07 14:54:42 -0800

    install rainbow-modec

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
                            smartparens clojure-mode-extra-font-locking cider
                            magit linum-relative json-mode exec-path-from-shell
                            flycheck haskell-mode circe debbugs ac-helm geiser
-                           ac-geiser multiple-cursors expand-region))
+                           ac-geiser multiple-cursors expand-region rainbow-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -94,15 +94,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
  '(custom-safe-themes
    (quote
-    ("874ae997df86ee4f08f2ddf24773c7a4e6b2ef702aa67ae38517aa1b67cd86fb" "a610de13501b8698429995d92d7ebff32ab57df4683fc5d6911bb46f223f5e33" "a1b538af356ee0c961f4063883610d103794301b302f77e8f6463802f1bcbc4c" "3be46993ab66e5677f5917de78acac3bc2979a6618427a2471faa24bfa652b79" "e5cfeed2030181c15b7a966ccb163c632d5ba70ebdf7d254e4a4f99455d5844f" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" default)))
+    ("116e71cb86b1f29be10cca6442a5e396327d772dad5cb83a9a9d33416f282603" "874ae997df86ee4f08f2ddf24773c7a4e6b2ef702aa67ae38517aa1b67cd86fb" "a610de13501b8698429995d92d7ebff32ab57df4683fc5d6911bb46f223f5e33" "a1b538af356ee0c961f4063883610d103794301b302f77e8f6463802f1bcbc4c" "3be46993ab66e5677f5917de78acac3bc2979a6618427a2471faa24bfa652b79" "e5cfeed2030181c15b7a966ccb163c632d5ba70ebdf7d254e4a4f99455d5844f" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" default)))
  '(fci-rule-color "#383838")
  '(package-selected-packages
    (quote
-    (expand-region multiple-cursors ac-geiser geiser ac-helm debbugs circe haskell-mode flycheck exec-path-from-shell json-mode company linum-relative magit cider clojure-mode-extra-font-locking smartparens rainbow-delimiters evil-surround evil-paredit paredit evil)))
+    (rainbow-mode expand-region multiple-cursors ac-geiser geiser ac-helm debbugs circe haskell-mode flycheck exec-path-from-shell json-mode company linum-relative magit cider clojure-mode-extra-font-locking smartparens rainbow-delimiters evil-surround evil-paredit paredit evil)))
  '(safe-local-variable-values
    (quote
     ((eval when
