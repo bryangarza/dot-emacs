60456966858524b1f206c52e2469631f8d047655
Bryan Garza <brygarza@gmail.com>
2015-03-28 14:59:46 -0700

    Add function to replace curly with straight quotes

--- init.el
+++ init.el
@@ -632,3 +632,22 @@ want to use in the modeline *in lieu of* the original.")
 
 (require 'smooth-scrolling)
 (setq smooth-scroll-margin 5)
+
+(setq doc-view-continuous t)
+
+(defun replace-smart-quotes (beg end)
+  "Replace 'smart quotes' in buffer or region with ascii quotes."
+  (interactive "r")
+  (format-replace-strings '(("\x201C" . "\"")
+                            ("\x201D" . "\"")
+                            ("\x2018" . "'")
+                            ("\x2019" . "'"))
+                          nil beg end))
+
+(defun yank-and-replace-smart-quotes ()
+  "Yank (paste) and replace smart quotes from the source with ascii quotes."
+  (interactive)
+  (yank)
+  (replace-smart-quotes (mark) (point)))
+
+(global-set-key (kbd "C-c y") 'yank-and-replace-smart-quotes)
