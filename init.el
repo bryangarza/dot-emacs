f36c0072de6cd372139b18db7318671419c599f3
Bryan Garza <brygarza@gmail.com>
2015-05-24 19:23:38 -0700

    rcirc settings

--- init.el
+++ init.el
@@ -442,13 +442,14 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     (setq-default save-place t)
     (setq save-place-file "~/.emacs.d/saved-places")))
 
+;; IRC auth
+(load-file "~/.private.el")
 ;; https://github.com/howardabrams/dot-files/blob/master/emacs-irc.org
 ;; https://github.com/jorgenschaefer/circe/wiki/Configuration
 (use-package circe
   :ensure t
   :config
   (progn
-    (load-file "~/.private.el")
     (setq circe-network-options
           `(("Freenode"
              :tls t
@@ -782,3 +783,34 @@ want to use in the modeline *in lieu of* the original.")
   :ensure t)
 
 (add-hook 'hy-mode-hook 'paredit-mode)
+
+(setq rcirc-server-alist
+      '(("irc.freenode.net" :port 6697 :encryption tls
+	 :channels ("#haskell" "#emacs"))))
+
+(setq rcirc-authinfo '(("freenode" nickserv "wolfcore" ,freenode-password)))
+
+(defun-rcirc-command reconnect (arg)
+  "Reconnect the server process."
+  (interactive "i")
+  (if (buffer-live-p rcirc-server-buffer)
+      (with-current-buffer rcirc-server-buffer
+        (let ((reconnect-buffer (current-buffer))
+              (server (or rcirc-server rcirc-default-server))
+              (port (if (boundp 'rcirc-port) rcirc-port rcirc-default-port))
+              (nick (or rcirc-nick rcirc-default-nick))
+              channels)
+          (dolist (buf (buffer-list))
+            (with-current-buffer buf
+              (when (equal reconnect-buffer rcirc-server-buffer)
+                (remove-hook 'change-major-mode-hook
+                             'rcirc-change-major-mode-hook)
+                (let ((server-plist (cdr (assoc-string server rcirc-server-alist))))
+                  (when server-plist
+                    (setq channels (plist-get server-plist :channels))))
+                )))
+          (if process (delete-process process))
+          (rcirc-connect server port nick
+                         nil
+                         nil
+                         channels)))))
