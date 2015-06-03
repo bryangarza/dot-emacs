a30e8d3852e528deb4e7112042f68bcb768b25da
Bryan Garza <brygarza@gmail.com>
2015-06-02 22:35:23 -0700

    Change faces for HTML current tag and current column

--- custom.el
+++ custom.el
@@ -23,4 +23,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-literate-comment-face ((t (:background "black" :foreground "#fcfcfc")))))
+ '(haskell-literate-comment-face ((t (:background "black" :foreground "#fcfcfc"))))
+ '(web-mode-current-column-highlight-face ((t (:background "#3e3c36" :foreground "Orange")))))
