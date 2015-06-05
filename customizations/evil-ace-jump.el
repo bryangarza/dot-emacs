084e30a17754e87312dea1aa1eefa9eb89b4a02e
Bryan Garza <brygarza@gmail.com>
2015-06-05 11:58:51 -0700

    Use evil ace jump mode integration
    Also enable the powerful jump back function.

---
+++ customizations/evil-ace-jump.el
@@ -0,0 +1,70 @@
+;; AceJump integration is now included in evil, this gist is only preserved for historical reasons.
+;; Please use the provided integration (it's far more advanced)
+
+;; AceJump is a nice addition to evil's standard motions.
+
+;; The following definitions are necessary to define evil motions for ace-jump-mode (version 2).
+
+;; ace-jump is actually a series of commands which makes handling by evil
+;; difficult (and with some other things as well), using this macro we let it
+;; appear as one.
+
+(defmacro evil-enclose-ace-jump (&rest body)
+  `(let ((old-mark (mark))
+         (ace-jump-mode-scope 'window))
+     (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
+     (remove-hook 'post-command-hook #'evil-visual-post-command t)
+     (unwind-protect
+         (progn
+           ,@body
+           (recursive-edit))
+       (if (evil-visual-state-p)
+           (progn
+             (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
+             (add-hook 'post-command-hook #'evil-visual-post-command nil t)
+             (set-mark old-mark))
+         (push-mark old-mark)))))
+
+(evil-define-motion evil-ace-jump-char-mode (count)
+  :type exclusive
+  (evil-enclose-ace-jump
+   (ace-jump-mode 5)))
+
+(evil-define-motion evil-ace-jump-line-mode (count)
+  :type line
+  (evil-enclose-ace-jump
+   (ace-jump-mode 9)))
+
+(evil-define-motion evil-ace-jump-word-mode (count)
+  :type exclusive
+  (evil-enclose-ace-jump
+   (ace-jump-mode 1)))
+
+(evil-define-motion evil-ace-jump-char-to-mode (count)
+  :type exclusive
+  (evil-enclose-ace-jump
+   (ace-jump-mode 5)
+   (forward-char -1)))
+
+(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
+
+;; some proposals for binding:
+
+(define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode)
+(define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-word-mode)
+
+(define-key evil-operator-state-map (kbd "SPC") #'evil-ace-jump-char-mode)      ; similar to f
+(define-key evil-operator-state-map (kbd "C-SPC") #'evil-ace-jump-char-to-mode) ; similar to t
+(define-key evil-operator-state-map (kbd "M-SPC") #'evil-ace-jump-word-mode)
+
+;; different jumps for different visual modes
+(defadvice evil-visual-line (before spc-for-line-jump activate)
+  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-line-mode))
+
+(defadvice evil-visual-char (before spc-for-char-jump activate)
+  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
+
+(defadvice evil-visual-block (before spc-for-char-jump activate)
+  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
+
+(provide 'evil-ace-jump)
--- init.el
+++ init.el
@@ -766,9 +766,18 @@ want to use in the modeline *in lieu of* the original.")
     "ace-jump-mode"
     "Emacs quick move minor mode"
     t))
-
 (bind-key "C-c SPC" 'ace-jump-mode)
-(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+(require 'evil-ace-jump)
 
 ;; Org-present, keys are:
 
