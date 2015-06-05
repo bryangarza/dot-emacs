5d579ea11780d7b20b82fc0680f37b8b89a2e4b7
Bryan Garza <brygarza@gmail.com>
2015-06-05 13:37:41 -0700

    Move cider

--- init.el
+++ init.el
@@ -124,16 +124,7 @@
     ;; (set-face-attribute 'show-paren-match-face nil :weight 'medium :underline nil)
     (setq show-paren-delay 0)))
 
-;; Cider settings
-;; Enable `eldoc` in Clojure buffers
-(add-hook 'cider-mode-hool #'eldoc-mode)
-;; Log communication with the nREPL server
-(setq nrepl-log-messages t)
-;; Hide *nrepl-connection* and *nrepl-server* from some buffer switching
-;; comands like `switch-to-buffer` (C-x b)
-(setq nrepl-hide-special-buffers t)
-;; Enable paredit in the REPL buffer
-(add-hook 'cider-repl-mode-hook #'paredit-mode)
+(require 'bryan-cider)
 
 (use-package auto-complete
   :ensure t
