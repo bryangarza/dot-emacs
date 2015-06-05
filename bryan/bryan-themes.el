fa5d017a641c2f5e20502ccf94dcf903fe19fdc7
Bryan Garza <brygarza@gmail.com>
2015-06-05 13:30:29 -0700

    Move themes to their own config file

---
+++ bryan/bryan-themes.el
@@ -0,0 +1,34 @@
+;; themes.el
+
+(defadvice load-theme
+  (before theme-dont-propagate activate)
+  (mapc #'disable-theme custom-enabled-themes))
+
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/noctilux")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/cyberpunk-theme.el")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/color-theme-ujelly")
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/moe-theme.el/")
+(add-to-list 'load-path "~/.emacs.d/themes/moe-theme.el/")
+;; so kawaii~  ✿◕ ‿ ◕✿
+(require 'moe-theme)
+;; (moe-light)
+(setq moe-theme-highlight-buffer-id nil)
+(moe-light)
+(moe-theme-set-color 'green)
+
+;; no underlined text! include `:weight 'normal` to get rid of bold
+;; (but who would wanna do that?)
+;; another example: (set-face-bold-p 'bold nil)
+(mapc
+  (lambda (face)
+    (set-face-attribute face nil :underline nil))
+  (face-list))
+;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
+
+;; (set-frame-parameter nil 'background-mode 'dark)
+;; (load-theme 'cyberpunk t)
+;; (load-theme 'solarized t)
+
+(provide 'bryan-themes)
