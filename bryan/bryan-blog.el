e66feb164ce30eae1082ea13b5f919278801e869
Bryan Garza <brygarza@gmail.com>
2015-06-23 23:53:34 -0700

    Overhaul organization of emacs blog

--- bryan/bryan-blog.el
+++ bryan/bryan-blog.el
@@ -2,43 +2,7 @@
 
 (setq org-publish-project-alist
       '(("blog"
-         :base-directory "~/bryangarza.github.io/org/blog/"
-         :publishing-directory "~/bryangarza.github.io/blog/"
-         :recursive t
-
-         :base-extension "org"
-         :html-extension "html"
-
-         :publishing-function (org-html-publish-to-html)
-
-         :html-preamble nil
-         :html-postamble "<div id=\"footer\">
-<div id=\"footer-hr\"></div>
-<p><a href=\"https://github.com/bryangarza/\">GitHub</a>, <a href=\"https://twitter.com/bryangarza\">Twitter</a>, <a href=\"https://medium.com/@bryangarza\">Medium</a>, and <a href=\"https://www.quora.com/Bryan-Garza\">Quora</a>. <a href=\"contact.html\"><strong>Contact</strong></a> me!</p></div>"
-
-         :html-doctype "html5"
-         :with-toc nil
-         :section-numbers nil
-         :html-head-include-default-style nil
-         :html-head-include-scripts nil
-
-         :html-head "<meta charset=\"utf-8\">
-<meta name=\"viewport\" content=\"width=560\">
-
-<link rel=\"stylesheet\" href=\"../css/normalize.css\" type=\"text/css\" />
-<link rel=\"stylesheet\" href=\"../css/org.css\" type=\"text/css\" />
-<link rel=\"stylesheet\" href=\"../css/animate.min.css\" type=\"text/css\" />"
-
-         :auto-sitemap t
-         :sitemap-title "Blog"
-         :sitemap-filename "blogmap.org"
-         :sitemap-sort-files anti-chronologically
-         :sitemap-file-entry-format "%t → %d" ;write title and date in sitemap
-         :sitemap-date-format "%B %d, %Y"
-         )
-
-        ("pages"
-         :base-directory "~/bryangarza.github.io/org/pages/"
+         :base-directory "~/bryangarza.github.io/org/"
          :publishing-directory "~/bryangarza.github.io/"
          :recursive t
 
@@ -48,12 +12,15 @@
          :publishing-function (org-html-publish-to-html)
 
          :html-preamble nil
-         :html-postamble "<div id=\"footer\">
+         :html-postamble "
+<div id=\"footer\">
 <div id=\"footer-hr\"></div>
-<p><a href=\"https://github.com/bryangarza/\">GitHub</a>, <a href=\"https://twitter.com/bryangarza\">Twitter</a>, <a href=\"https://medium.com/@bryangarza\">Medium</a>, and <a href=\"https://www.quora.com/Bryan-Garza\">Quora</a>. <a href=\"contact.html\"><strong>Contact</strong></a> me!</p></div>"
+<p>
+∴ <a href=\"/\">Home</a>, <a href=\"about.html\">About</a>, <a href=\"contact.html\">Contact</a>, <a href=\"https://github.com/bryangarza/\">GitHub</a>, <a href=\"https://twitter.com/bryangarza\">Twitter</a>, and <a href=\"https://www.quora.com/Bryan-Garza\">Quora</a></p></div>"
 
          :html-doctype "html5"
          :with-toc nil
+         :with-timestamps t
          :section-numbers nil
          :html-head-include-default-style nil
          :html-head-include-scripts nil
@@ -63,7 +30,7 @@
 
 <link rel=\"stylesheet\" href=\"css/normalize.css\" type=\"text/css\" />
 <link rel=\"stylesheet\" href=\"css/org.css\" type=\"text/css\" />
-<link rel=\"stylesheet\" href=\"css/animate.min.css\" type=\"text/css\" />"
-         )))
+<link rel=\"stylesheet\" href=\"css/tables.css\" type=\"text/css\" />"
 
-(provide 'bryan-blog)
+         :auto-sitemap nil
+         )))
