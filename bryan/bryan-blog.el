3a687a4b66cd31223128750c61b8621cdc71f6bf
Bryan Garza <brygarza@gmail.com>
2015-06-24 01:16:58 -0700

    Remove extra whitespace

--- bryan/bryan-blog.el
+++ bryan/bryan-blog.el
@@ -12,11 +12,9 @@
          :publishing-function (org-html-publish-to-html)
 
          :html-preamble nil
-         :html-postamble "
-<div id=\"footer\">
-<div id=\"footer-hr\"></div>
+         :html-postamble "<footer>
 <p>
-∴ <a href=\"/\">Home</a>, <a href=\"about.html\">About</a>, <a href=\"contact.html\">Contact</a>, <a href=\"https://github.com/bryangarza/\">GitHub</a>, <a href=\"https://twitter.com/bryangarza\">Twitter</a>, and <a href=\"https://www.quora.com/Bryan-Garza\">Quora</a></p></div>"
+∴ <a href=\"/\">Home</a>, <a href=\"about.html\">About</a>, <a href=\"contact.html\">Contact</a>, <a href=\"https://github.com/bryangarza/\">GitHub</a>, <a href=\"https://twitter.com/bryangarza\">Twitter</a>, and <a href=\"https://www.quora.com/Bryan-Garza\">Quora</a></p></footer>"
 
          :html-doctype "html5"
          :with-toc nil
@@ -27,10 +25,8 @@
 
          :html-head "<meta charset=\"utf-8\">
 <meta name=\"viewport\" content=\"width=560\">
-
 <link rel=\"stylesheet\" href=\"css/normalize.css\" type=\"text/css\" />
 <link rel=\"stylesheet\" href=\"css/org.css\" type=\"text/css\" />
 <link rel=\"stylesheet\" href=\"css/tables.css\" type=\"text/css\" />"
 
-         :auto-sitemap nil
-         )))
+         :auto-sitemap nil)))
