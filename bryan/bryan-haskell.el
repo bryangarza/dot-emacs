78a06b886bf8a99d11f1765eda480f80358a5822
Bryan Garza <brygarza@gmail.com>
2015-06-26 14:34:39 -0700

    Add haskell settings

--- bryan/bryan-haskell.el
+++ bryan/bryan-haskell.el
@@ -13,7 +13,34 @@
   ;; (paredit-mode 1)
   (turn-on-hi2)
   (inf-haskell-mode)
-  (electric-indent-mode nil))
+  (electric-indent-mode nil)
+
+  ;; Load the current file (and make a session if not already made).
+  (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
+  ;; Switch to the REPL.
+  (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
+  ;; “Bring” the REPL, hiding all other windows apart from the source
+  ;; and the REPL.
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  ;; Get the type and info of the symbol at point, print it in the
+  ;; message buffer.
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  ;; Contextually do clever things on the space key, in particular:
+  ;;   1. Complete imports, letting you choose the module name.
+  ;;   2. Show the type of the symbol after the space.
+  (evil-leader/set-key
+    "SPC" 'haskell-mode-contextual-space
+    "s" 'haskell-mode-tag-find)
+  ;; Jump to the imports. Keep tapping to jump between import
+  ;; groups. C-u f8 to jump back again.
+  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+
+  ;; (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  ;; ;; Interactively choose the Cabal command to run.
+  ;; (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  )
+
 
 (use-package haskell-mode
   :ensure t
