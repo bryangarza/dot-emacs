5798bd38518601a174179d76168debc43df6b4af
Bryan Garza <brygarza@gmail.com>
2015-10-11 12:55:49 -0700

    Update themes

--- bryan/bryan-themes.el
+++ bryan/bryan-themes.el
@@ -7,17 +7,21 @@
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/noctilux")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/cyberpunk-theme.el")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/color-theme-ujelly")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-theme-darktooth")
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/moe-theme.el/")
 (add-to-list 'load-path "~/.emacs.d/themes/moe-theme.el/")
 ;; so kawaii~  ✿◕ ‿ ◕✿
+
+USUAL SETUP ;;;;;;;;;;;;
 (require 'moe-theme)
 (setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
 (setq moe-theme-highlight-buffer-id nil)
-(moe-theme-set-color 'blue)
+(moe-theme-set-color 'green)
 ;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
-;; (moe-light)
-(moe-dark)
+(moe-light)
+;; (moe-dark)
+
 
 ;; no underlined text! include `:weight 'normal` to get rid of bold
 ;; (but who would wanna do that?)
@@ -29,5 +33,6 @@
 
 ;; (set-frame-parameter nil 'background-mode 'dark)
 ;; (load-theme 'cyberpunk t)
+;; (load-theme 'darktooth t)
 
 (provide 'bryan-themes)
