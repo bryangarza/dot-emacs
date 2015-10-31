7af1756eb185876004581cfa196e5582cee70b97
Bryan Garza <brygarza@gmail.com>
2015-10-30 18:40:22 -0700

    Add commented-out racer, though it doesn't work

--- bryan/bryan-rust.el
+++ bryan/bryan-rust.el
@@ -3,4 +3,16 @@
 (use-package rust-mode
   :ensure t)
 
+;; (setq racer-rust-src-path "~/.emacs.d/lisp/racer/src/")
+;; (setq racer-cmd "~/.emacs.d/lisp/racer/target/release/racer")
+;; (add-to-list 'load-path "~/.emacs.d/lisp/racer/editors/emacs/")
+;; (eval-after-load "rust-mode" '(require 'racer))
+
+;; (defun rust-custom-hook ()
+;;   (racer-activate)
+;;   (local-set-key (kbd "M-.") #'racer-find-definition)
+;;   (local-set-key (kbd "TAB") #'racer-complete-or-indent))
+
+;; (add-hook 'rust-mode-hook 'rust-custom-hook)
+
 (provide 'bryan-rust)
