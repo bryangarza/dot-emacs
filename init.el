3b0bd119f79dbfe001808a0534130190741da60f
Bryan Garza <brygarza@gmail.com>
2016-01-23 11:13:00 -0800

    Formatting

--- init.el
+++ init.el
@@ -4,10 +4,8 @@
 (require 'cl)
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
 (setq package-enable-at-startup nil)
